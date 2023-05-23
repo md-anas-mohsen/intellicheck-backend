9399c60069a1a240ed0fb2473d613034678fcb5d
Anas Mohsen <m.mohsen_18634@khi.iba.edu.pk>
2023-05-23 14:38:55 +0500

    chore: remove pagination records limit

--- constants/common.js
+++ constants/common.js
@@ -8,7 +8,7 @@ exports.ORDER_BY_DIRECTIONS = {
 exports.PAGINATION_OPTIONS = {
   keyword: Joi.string().allow("").trim().optional(),
   page: Joi.number().empty("").default(1).optional(),
-  limit: Joi.number().empty("").default(20).max(500).optional(),
+  limit: Joi.number().empty("").default(20).optional(),
 };
 
 exports.MONGODB_OBJECT_ID_REGEX = /^(?=[a-f\d]{24}$)(\d+[a-f]|[a-f]+\d)/i;
